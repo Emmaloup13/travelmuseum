50d22994fd95739da5bb2e7469c868ec9953298c
Emma Bolmin <emmabolmin@yahoo.fr>
2023-04-16 11:00:20 +0200

    new dist for netlify

--- front/vue.config.js
+++ front/vue.config.js
@@ -6,9 +6,3 @@ module.exports = defineConfig({
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/travelmuseum/" : "/",
 };
-
-module.exports = {
-  devServer: {
-    headers: { "Cache-Control": "no-cache, no-store, must-revalidate", "X-Content-Type-Options": "nosniff" }
-  }
-}
